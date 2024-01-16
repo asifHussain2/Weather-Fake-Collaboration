43e6925f4ebff711614741fbac564fcac9a8de39
asifHussain2 <156398448+asifHussain2@users.noreply.github.com>
2024-01-16 19:03:19 +0500

    Add files via upload

---
+++ WeatherService.java
@@ -0,0 +1,16 @@
+package weather;
+
+public class WeatherService {
+private WeatherApiClient weatherApi;
+public WeatherService(WeatherApiClient weatherApi) {
+	this.weatherApi = weatherApi;
+}
+
+public String getCurrentWeather() {
+	return weatherApi.getCurrentWeather();
+}
+
+public String getForecast() {
+	return weatherApi.getForecast();
+}
+}
